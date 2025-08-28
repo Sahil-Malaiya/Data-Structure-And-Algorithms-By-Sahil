997fa4957a9c7ffee2c84f7086dbe1a766cb54a4
Sahil <sahilmalaiya7@gmail.com>
2025-08-28 21:02:25 +0530

    Today did Tabulation method in dp

--- FibonacciInDp.java
+++ FibonacciInDp.java
@@ -10,9 +10,20 @@ public class FibonacciInDp {
         return f[n];
     }
 
+    public static int fibTablutaion(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         int n = 5;
         int f[] = new int[n + 1];
-        System.out.print(fib(n, f));
+        System.out.println(fib(n, f));
+        System.out.println(fibTablutaion(n));
     }
 }
